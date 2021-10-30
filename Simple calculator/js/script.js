8ffe1a7d752e4dbeada1ce731d493bbc380c46db
Vaceslav Ignatuk <vaceslavignatuk@gmail.com>
2021-10-30 19:01:41 +0300

    fixed result field

--- Simple calculator/js/script.js
+++ Simple calculator/js/script.js
@@ -4,35 +4,35 @@ function sum() {
 	let a = document.getElementById('input1').value;
 	let b = document.getElementById('input2').value;
 	let res = +a + +b;
-	document.getElementById('result').innerHTML = res;
+	document.getElementById('result').innerHTML = res.toFixed(2);
 }
 
 function subtraction() {
 	let a = document.getElementById('input1').value;
 	let b = document.getElementById('input2').value;
 	let res = a - b;
-	document.getElementById('result').innerHTML = res;
+	document.getElementById('result').innerHTML = res.toFixed(2);
 }
 
 function multiplication() {
 	let a = document.getElementById('input1').value;
 	let b = document.getElementById('input2').value;
 	let res = a * b;
-	document.getElementById('result').innerHTML = res;
+	document.getElementById('result').innerHTML = res.toFixed(2);
 }
 
 function division() {
 	let a = document.getElementById('input1').value;
 	let b = document.getElementById('input2').value;
 	let res = a / b;
-	document.getElementById('result').innerHTML = res;
+	document.getElementById('result').innerHTML = res.toFixed(2);
 }
 
 function exponentiation() {
 	let a = document.getElementById('input1').value;
 	let b = document.getElementById('input2').value;
 	let res = a ** b;
-	document.getElementById('result').innerHTML = res;
+	document.getElementById('result').innerHTML = res.toFixed(2);
 }
 
 function reset() {
